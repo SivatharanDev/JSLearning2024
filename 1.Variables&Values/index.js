2a647a3d6f96fc875eeecff74123e5df8997e8ca
SivatharanDev <sivatharan2025@gmail.com>
2024-02-17 19:47:05 +0530

    challenge 3 and challenge 4

---
+++ 1.Variables&Values/index.js
@@ -0,0 +1,10 @@
+let country = 'SriLanka';
+let continent = 'Asia';
+let population = 22.16;
+
+console.log(`Hello I'm from ${country} and its in the ${continent} continent. total population of my country is ${population} million in 2024.`);
+
+let job = null;
+
+console.log(job);
+console.log(typeof job);
---
+++ Challenge3/index.js
@@ -0,0 +1,10 @@
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy 🎉");
+} else if (scoreDolphins < scoreKoalas) {
+    console.log("Koalas win the trophy 🎉");
+} else {
+    console.log("Both win the trophy 🎉");
+}
